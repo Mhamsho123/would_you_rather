64386348d3f4a3b8b265aebd3610b43823580de5
Mohamad Hamsho <hamsho1030@gmail.com>
2026-04-08 15:48:54 -0400

    Added data file

---
+++ data.js
@@ -0,0 +1,77 @@
+export const wouldYouRatherQuestions = [
+    {
+      id: 1,
+      question: "Would you rather be able to fly or be invisible?",
+      choices: ["Fly", "Be invisible"]
+    },
+    {
+      id: 2,
+      question: "Would you rather live in the mountains or by the beach?",
+      choices: ["Live in the mountains", "Live by the beach"]
+    },
+    {
+      id: 3,
+      question: "Would you rather always have free food or free travel?",
+      choices: ["Free food", "Free travel"]
+    },
+    {
+      id: 4,
+      question: "Would you rather give up your phone for a year or your favorite food for a year?",
+      choices: ["Give up my phone", "Give up my favorite food"]
+    },
+    {
+      id: 5,
+      question: "Would you rather be super strong or super smart?",
+      choices: ["Super strong", "Super smart"]
+    },
+    {
+      id: 6,
+      question: "Would you rather only watch movies or only listen to music for a year?",
+      choices: ["Only watch movies", "Only listen to music"]
+    },
+    {
+      id: 7,
+      question: "Would you rather have unlimited money or unlimited time?",
+      choices: ["Unlimited money", "Unlimited time"]
+    },
+    {
+      id: 8,
+      question: "Would you rather explore space or explore the deep ocean?",
+      choices: ["Explore space", "Explore the deep ocean"]
+    },
+    {
+      id: 9,
+      question: "Would you rather always be 10 minutes late or always 20 minutes early?",
+      choices: ["Always 10 minutes late", "Always 20 minutes early"]
+    },
+    {
+      id: 10,
+      question: "Would you rather never use social media again or never watch YouTube again?",
+      choices: ["Never use social media again", "Never watch YouTube again"]
+    },
+    {
+      id: 11,
+      question: "Would you rather have the ability to read minds or predict the future?",
+      choices: ["Read minds", "Predict the future"]
+    },
+    {
+      id: 12,
+      question: "Would you rather work your dream job for low pay or a boring job for high pay?",
+      choices: ["Dream job for low pay", "Boring job for high pay"]
+    },
+    {
+      id: 13,
+      question: "Would you rather never eat sweets again or never drink soda again?",
+      choices: ["Never eat sweets again", "Never drink soda again"]
+    },
+    {
+      id: 14,
+      question: "Would you rather be famous or be rich?",
+      choices: ["Be famous", "Be rich"]
+    },
+    {
+      id: 15,
+      question: "Would you rather live without AC in the summer or without heat in the winter?",
+      choices: ["No AC in the summer", "No heat in the winter"]
+    }
+    ];
